8938b4a387ca2148036bc46c5f52a551191fc463
ばたー <140896603+bata-san@users.noreply.github.com>
2026-07-29 10:38:30 +0900

    Type compact background proxy data

--- src/background-data.d.ts
+++ src/background-data.d.ts
@@ -3,4 +3,7 @@ declare module 'virtual:mmd-background-data' {
   export const BACKGROUND_SHA256: string;
   export const BACKGROUND_UNCOMPRESSED_BYTES: number;
   export const BACKGROUND_GZIP_BASE64_CHUNKS: readonly string[];
+  export const BACKGROUND_PROXY_SHA256: string;
+  export const BACKGROUND_PROXY_UNCOMPRESSED_BYTES: number;
+  export const BACKGROUND_PROXY_GZIP_BASE64: string;
 }
