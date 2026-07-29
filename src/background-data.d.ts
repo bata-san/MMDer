7f30e24b0490c0f2f4d807335b59e6a411dde2d6
ばたー <140896603+bata-san@users.noreply.github.com>
2026-07-29 10:09:40 +0900

    Declare private background module

---
+++ src/background-data.d.ts
@@ -0,0 +1,6 @@
+declare module 'virtual:mmd-background-data' {
+  export const BACKGROUND_NAME: string;
+  export const BACKGROUND_SHA256: string;
+  export const BACKGROUND_UNCOMPRESSED_BYTES: number;
+  export const BACKGROUND_GZIP_BASE64_CHUNKS: readonly string[];
+}
